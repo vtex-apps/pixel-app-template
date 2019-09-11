7976ca0a8091fdaaf22f1828e2891ae73fc33fd8
Pablo Martin <pmartin@webimpacto.es>
2019-09-11 15:50:20 +0200

    Fix import PixelMessage from typings/events.d.ts

--- react/index.tsx
+++ react/index.tsx
@@ -1,5 +1,7 @@
 import { canUseDOM } from 'vtex.render-runtime'
 
+import { PixelMessage } from './typings/events'
+
 export function handleEvents(e: PixelMessage) {
   switch (e.data.eventName) {
     case 'vtex:pageView': {
